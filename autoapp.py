3c8dd9a66bc2517377f9b052dae08298db3ae004
Maxime Muhlethaler <maxime.muhlethaler@gmail.com>
2023-12-01 09:28:31 +0100

    images et correct

--- autoapp.py
+++ autoapp.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
 from animalguessinggame.app import create_app
-from watchdog.events import EVENT_TYPE_OPENED
+#from watchdog.events import EVENT_TYPE_OPENED
 
 app = create_app()
