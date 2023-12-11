87b4a360c5fb1aca1bf01ad1f4bf17d84e05df98
Maxime Muhlethaler <maxime.muhlethaler@gmail.com>
2023-12-11 16:02:30 +0100

    hachage

--- animalguessinggame/user/models.py
+++ animalguessinggame/user/models.py
@@ -33,7 +33,7 @@ class User(UserMixin, PkModel):
     __tablename__ = "users"
     username = Column(db.String(80), unique=True, nullable=False)
     email = Column(db.String(80), unique=True, nullable=False)
-    _password = Column("password", db.String(128), nullable=True)  # Change LargeBinary to String
+    _password = Column("password", db.LargeBinary(128), nullable=True)  # Change LargeBinary to String
     created_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
     first_name = Column(db.String(30), nullable=True)
     last_name = Column(db.String(30), nullable=True)
@@ -42,17 +42,17 @@ class User(UserMixin, PkModel):
 
     @hybrid_property
     def password(self):
-        """Plain text password."""
+        """Hashed password."""
         return self._password
 
     @password.setter
     def password(self, value):
         """Set password."""
-        self._password = value
+        self._password = bcrypt.generate_password_hash(value)
 
     def check_password(self, value):
         """Check password."""
-        return self._password == value
+        return bcrypt.check_password_hash(self._password, value)
 
     @property
     def full_name(self):
