0040c8c2c09793757348828ec88054b35aad370b
Maxime Muhlethaler <maxime.muhlethaler@gmail.com>
2023-12-22 15:28:57 +0100

    fond

--- autoapp.py
+++ autoapp.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
 from animalguessinggame.app import create_app
-#from watchdog.events import EVENT_TYPE_OPENED
-from flask import Flask
-
-
 app = create_app()
