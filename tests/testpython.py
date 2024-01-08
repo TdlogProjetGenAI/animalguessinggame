ebefa46a93a4e7312ca635de5ba7f0660d1157c5
Pottier Titouan <titouan.pottier22@gmail.com>
2024-01-08 15:31:55 +0100

    correction 2 test 1

--- tests/testpython.py
+++ tests/testpython.py
@@ -1,14 +1,14 @@
 """Test for python fonctions."""
 
-import unittest
-import UnitTest.classif_animals10
+import pytest
+import pytest.classif_animals10
 
 class TestConcat(unittest.TestCase):
     """Test for concat fonction."""
 
     def testconcat(self):
         """Test with 5236."""
-        r = UnitTest.classif_animals10.concat([5,2,3,6])
+        r = pytest.classif_animals10.concat([5,2,3,6])
         self.assertEqual(r, "cinq mille deux cent trente six")
 
 if __name__ == "__main__":
