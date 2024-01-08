6f6ce6c47628ae999542803bb049d92e53c4beac
Pottier Titouan <titouan.pottier22@gmail.com>
2024-01-08 15:12:08 +0100

    ajoute du premier test

---
+++ tests/testpython.py
@@ -0,0 +1,15 @@
+"""Test for python fonctions."""
+
+import unittest
+import UnitTest.classif_animals10 as mp
+
+class testconcat(unittest.TestCase):
+    """Test for concat fonction."""
+
+    def testConcat(self):
+        """Test with 5236."""
+        r = mp.concat([5,2,3,6])
+        self.assertEqual(r, "cinq mille deux cent trente six")
+
+if __name__ == "__main__":
+    unittest.main()
